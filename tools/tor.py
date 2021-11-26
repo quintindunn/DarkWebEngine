e49593ab282a35f0954d00aaada9dea65474432f
quintindunn <93884113+quintindunn@users.noreply.github.com>
2021-11-25 22:45:40 -0500

    Update tor.py

--- tools/tor.py
+++ tools/tor.py
@@ -4,7 +4,7 @@ from stem.control import Controller
 
 
 def renew_connection():
-    hashed_pwd = "HashedControlPassword 16:4885259C4E594EF260E9232AD3EBC19FFED5A7F0DF1E17E60DEA6C19D4"
+    hashed_pwd = "XXXX"
     with Controller.from_port(port=9151) as controller:
         controller.authenticate(password=hashed_pwd)
         controller.signal(Signal.NEWNYM)
